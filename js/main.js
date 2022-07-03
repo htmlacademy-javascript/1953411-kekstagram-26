5f88aa60463f6bb0b97c92c8998202460c2ce0aa
timeliercomb <levshin.v.99@gmail.com>
2022-07-03 21:37:00 +0300

    изменяет логику создания и рендера фото, вынося функции в главный модуль

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 import './util.js';
-import './data.js';
-import './picture.js';
-import './create-big-picture.js';
+import {createSimilarPhotoCards} from './data.js';
+
+const generatedPhotos = createSimilarPhotoCards(25);
+
+import {createSimilarPhotos, renderPhotos} from './picture.js';
+
+const pictures = createSimilarPhotos(generatedPhotos);
+renderPhotos(pictures);
+
+// import './create-big-picture.js';
 import {addUploadPopupEventListener} from './open-close-download-image-popup.js';
 
 addUploadPopupEventListener();
