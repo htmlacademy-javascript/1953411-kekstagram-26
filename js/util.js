62703de2f552bf41375fdfe2fc47c6ee5786050c
timeliercomb <levshin.v.99@gmail.com>
2022-07-12 16:04:42 +0300

    добавляет функции debounce и throttle

--- js/util.js
+++ js/util.js
@@ -22,4 +22,29 @@ const createNewElement = (tagName, className, textContent) => {
   return newTag;
 };
 
-export {checkStringLength, getRandomPositiveInteger, createNewElement};
+function debounce (callback, timeoutDelay = 500) {
+
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+function throttle (callback, delayBetweenFrames) {
+
+  let lastTime = 0;
+
+  return (...rest) => {
+    const now = new Date();
+
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
+export {checkStringLength, getRandomPositiveInteger, createNewElement, debounce, throttle};
